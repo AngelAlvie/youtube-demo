ef6b3d439516571f8c4fbd380333841ef58b48f6
Angel Alvarez <angel.alvareza@affectiva.com>
2018-06-11 09:41:15 -0400

    Changed demo to load from the CDN asynchronously to improve page loading performance

--- dev/public/javascripts/index.js
+++ dev/public/javascripts/index.js
@@ -1,4 +1,17 @@
-var JSSDKDemo = (function() {
+$(document).ready(function() {
+    var isChrome = !!window.chrome && !!window.chrome.webstore;
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+    if (isChrome || isFirefox || isOpera) {
+        JSSDKDemo.init();
+        JSSDKDemo.run();
+    } else {
+        JSSDKDemo.create_alert("incompatible-browser", "It appears that you are using an unsupported browser. Please try this demo on Chrome, Firefox, or Opera.");
+    }
+});
+
+var JSSDKDemo = (function () {
     var detector = null;
     var capture_frames = false;
     var ready_to_accept_input = true;
@@ -589,16 +602,3 @@ var JSSDKDemo = (function() {
         create_alert: create_alert
     };
 })();
-
-$(document).ready(function() {
-    var isChrome = !!window.chrome && !!window.chrome.webstore;
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-    if (isChrome || isFirefox || isOpera) {
-        JSSDKDemo.init();
-        JSSDKDemo.run();
-    } else {
-        JSSDKDemo.create_alert("incompatible-browser", "It appears that you are using an unsupported browser. Please try this demo on Chrome, Firefox, or Opera.");
-    }
-});
