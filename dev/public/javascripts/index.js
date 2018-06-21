35432a99df7655d012be60224bd7b5660cf13cf7
Angel Alvarez <angel.alvareza@affectiva.com>
2018-06-21 10:56:40 -0400

    Made tweaks to the timing of the no face alert, also removed browser check for testing purposes

--- dev/public/javascripts/index.js
+++ dev/public/javascripts/index.js
@@ -11,7 +11,7 @@ const browserCheck = () => {
 
 $(document).ready(() => {
   JSSDKDemo = new Demo();
-  if (browserCheck()) {
+  if (/* browserCheck() reinsert after the changes have been made*/true) {
     JSSDKDemo.start();
   } else {
     JSSDKDemo.createAlert("incompatible-browser", "It appears that you are using an unsupported browser. Please try this demo on Chrome, Firefox, or Opera.");
@@ -44,6 +44,9 @@ function Demo() {
   // Internal state for the detector
   let time_buffering_ms = 0;
   let frames_since_last_face = 0;
+  const face_alert_threshold = 20;
+  const alert_transition_delay_in = 300;
+  const alert_transition_delay_out = 175;
   let face_visible = true;
   let detector = null;
 
@@ -77,7 +80,7 @@ function Demo() {
    * @param {string} id - Id of the html object that we cast the alert to
    * @param {string} text - text of the alert that we show to the user. */
   this.createAlert = (id, text) => {
-    $("#lightbox").fadeIn(500);
+    $("#lightbox").fadeIn(alert_transition_delay_in);
     $("<div></div>", {
       id: id,
       class: "alert alert-danger",
@@ -85,7 +88,7 @@ function Demo() {
       text: text,
     }).appendTo("#lightbox");
     $("#" + id).css({"text-align": "center", "z-index": 2});
-    $("#" + id).fadeIn(1000);
+    $("#" + id).fadeIn(alert_transition_delay_in);
   };
 
   /** ==============================================================
@@ -132,7 +135,7 @@ function Demo() {
           // account for time spent buffering
           const fake_timestamp = getCurrentTimeAdjusted();
           
-          if (frames_since_last_face > 100 && face_visible) {
+          if (frames_since_last_face > face_alert_threshold && face_visible) {
             face_visible = false;
             self.createAlert("no-face", "No face was detected. Please re-position your face and/or webcam.");
           }
@@ -557,10 +560,10 @@ function Demo() {
   const fadeAndRemove = (id) => {
     let removeObj = $(id);
 
-    removeObj.fadeOut(500, () => {
+    removeObj.fadeOut(alert_transition_delay_out, () => {
       removeObj.remove();
     });
-    $("#lightbox").fadeOut(1000);
+    $("#lightbox").fadeOut(alert_transition_delay_out);
   };
 
   /** Create an alert that tells the user that there is no internet connection. */
